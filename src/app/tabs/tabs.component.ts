74a6191e215af828d50744ef3756bbe1d9469cd1
mercklyzer <mercklyzer@gmail.com>
2022-04-09 23:29:52 +0800

    Hide tabs if user is not logged in

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import {filter} from 'rxjs/operators';
+import { User } from '../models/user.model';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-tabs',
@@ -9,10 +11,13 @@ import {filter} from 'rxjs/operators';
 })
 export class TabsComponent implements OnInit {
 
-  constructor(private router:Router ) {
-  }
+  constructor(
+    private router:Router,
+    private userService:UserService 
+  ){}
 
   url:string = ''
+  user!:User
 
   ngOnInit(): void {
     this.router.events.pipe(
@@ -20,7 +25,7 @@ export class TabsComponent implements OnInit {
     .subscribe((receivedEvent) => {
       if(receivedEvent instanceof NavigationEnd){
         this.url = receivedEvent.url;
-        console.log(this.url);
+        this.user = localStorage.getItem('User')? JSON.parse(localStorage.getItem('User')!) : ''
       }
     });
   }
